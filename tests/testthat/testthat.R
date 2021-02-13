9013c23a6362b02e626ca589b04f80dedb8e47ac
AnoushiravanR <anooshrz@yahoo.com>
2021-02-13 09:21:50 +0330

    modifying testthat.R

--- tests/testthat/testthat.R
+++ tests/testthat/testthat.R
@@ -13,9 +13,3 @@ test_that("The data preparation process has been successful", {
   expect_true(class(NOAA_data$Latitude) == "numeric")
 })
 
-
-test_that("Throwing error if the annot_col value in not a character strings", {
-  expect_error(eq_map(NOAA_data, annot_col = Date),
-  "annot_col argument needs to be a character string")
-})
-
